b9148347fd6faa7467b7cfe487ae6e620bba1779
jguidosantos-collab <jguidosantos@gmail.com>
2026-09-10 01:32:08 -0300

    Update rng.js

--- js/01. ENGINE/core/rng.js
+++ js/01. ENGINE/core/rng.js
@@ -35,7 +35,6 @@ class RNGCore {
 
         for (let i = arr.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
-
             [arr[i], arr[j]] = [arr[j], arr[i]];
         }
 
@@ -43,4 +42,4 @@ class RNGCore {
     }
 }
 
-const RNG = new RNGCore();
+window.RNG = new RNGCore();
