f804c64823a8da70a529ed40a7a1a9706e98185c
jguidosantos-collab <jguidosantos@gmail.com>
2026-09-10 01:28:12 -0300

    Update rng.js

--- js/01. ENGINE/core/rng.js
+++ js/01. ENGINE/core/rng.js
@@ -1,4 +1,4 @@
-class RNG {
+class RNGCore {
     random(min = 0, max = 1) {
         return Math.random() * (max - min) + min;
     }
@@ -18,25 +18,29 @@ class RNG {
     weightedChoice(options) {
         const total = options.reduce((sum, opt) => sum + opt.weight, 0);
         let random = this.random(0, total);
-        
+
         for (let option of options) {
             random -= option.weight;
+
             if (random <= 0) {
                 return option.value;
             }
         }
-        
+
         return options[options.length - 1].value;
     }
 
     shuffle(array) {
         const arr = [...array];
+
         for (let i = arr.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
+
             [arr[i], arr[j]] = [arr[j], arr[i]];
         }
+
         return arr;
     }
 }
 
-const RNG = new RNG();
+const RNG = new RNGCore();
