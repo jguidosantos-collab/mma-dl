50cc21fd7f73f4dfbe3d7ab98a0809348b32879c
jguidosantos-collab <jguidosantos@gmail.com>
2026-09-09 23:57:06 -0300

    Create events.js

---
+++ js/01. ENGINE/core/events.js
@@ -0,0 +1,26 @@
+class EventSystem {
+    constructor() {
+        this.listeners = {};
+    }
+
+    on(event, callback) {
+        if (!this.listeners[event]) {
+            this.listeners[event] = [];
+        }
+        this.listeners[event].push(callback);
+    }
+
+    off(event, callback) {
+        if (this.listeners[event]) {
+            this.listeners[event] = this.listeners[event].filter(cb => cb !== callback);
+        }
+    }
+
+    emit(event, data) {
+        if (this.listeners[event]) {
+            this.listeners[event].forEach(callback => callback(data));
+        }
+    }
+}
+
+const EventSystem = new EventSystem();
